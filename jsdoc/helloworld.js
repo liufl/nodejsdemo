91c1ddee3e585809c2965b9de40d04e373f50d30
liufeilong <liufl@zhihenget.com>
2015-09-01 10:44:04 +0800

    jsdoc

---
+++ jsdoc/helloworld.js
@@ -0,0 +1,51 @@
+/**
+变量 name
+*/
+var name = 'zhangsan';
+
+/*
+变量 name
+*/
+var sex = 'boy';
+
+// 变量 name
+var age = '18';
+
+/**
+cmd中
+使用node helloworld.js命令
+打印出 hello world
+*/
+console.log('hello world');
+/*
+cmd中
+使用node helloworld.js命令
+打印出 hello world
+*/
+console.log('hello world');
+
+// cmd中
+// 使用node helloworld.js命令
+// 打印出 hello world
+console.log('hello world');
+
+/**
+公开模块
+name printhello
+*/
+exports.printhello = function () {
+	console.log('hello');
+}
+/*
+公开模块
+name printhello
+*/
+exports.printhello = function () {
+	console.log('hello');
+}
+
+// 公开模块
+// name printhello
+exports.printhello = function () {
+	console.log('hello');
+}
